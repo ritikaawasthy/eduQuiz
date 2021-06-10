bef4f1dfc47f89265f2f441823997ff904cc75b6
ritikaawasthy <ritika.awasthy@gmail.com>
2021-06-10 19:54:20 +0530

    heroku settings

--- eduQuiz/settings.py
+++ eduQuiz/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
