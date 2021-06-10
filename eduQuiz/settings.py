30f897373714d03a15a8d805213baa5a3948c076
ritikaawasthy <ritika.awasthy@gmail.com>
2021-06-10 18:12:26 +0530

    deployment settings

--- eduQuiz/settings.py
+++ eduQuiz/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ev8d8y42-$m^u1&$+c7)0u*q-jxd@hvhrz_+5mqw%c$v_=q0vb'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
